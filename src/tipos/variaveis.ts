d57cf0955692533a5643233bfb40356cd4b20ffa
Miqueias Mesquita <miqueias_jed@hotmail.com>
2024-12-05 15:02:49 -0300

    basic types

---
+++ src/tipos/variaveis.ts
@@ -0,0 +1,20 @@
+const nome:string = "Miqueias"
+console.log(nome)
+
+let estaChovendo:boolean 
+estaChovendo = true
+console.log(estaChovendo)
+
+const idade:number = 27
+console.log(idade)
+
+let variavel: any = 4
+variavel = "texto"
+variavel = false
+variavel = {nome: "texto"}
+console.log(variavel)
+
+let misterio 
+misterio = 4
+misterio = "texto"
+console.log(misterio)
