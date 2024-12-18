d0f2042be3619d7b93bf7612b98c6863a5d69bd6
Miqueias Mesquita <miqueias_jed@hotmail.com>
2024-12-18 15:06:58 -0300

    validador

--- src/oo/classe_3.ts
+++ src/oo/classe_3.ts
@@ -1,10 +1,11 @@
+import Validador from "./Validador"
+
 class Produto{
 constructor(readonly id: number, readonly nome: string, readonly preco: number, readonly desc: number = 0){
-  if(id <= 0 ) throw new Error('id inválido')
-  if(nome.length < 0 ) throw new Error('Nome pequeno')
-  if(nome.length > 250 ) throw new Error('Nome grande')
-  if(preco <= 0 ) throw new Error('preço inválido')
-  if(desc < 0 || desc > 0.8) throw new Error('Desconto inválido')
+  Validador.maiorQueZero(id, 'Id inválido')
+  Validador.tamanhoEntre(nome, 3, 250, 'Nome inválido')
+  Validador.maiorQueZero(preco, 'Preço inválido')
+  Validador.entre(desc, 0, 0.8, 'Desconto inválido')
 }
   get precoFinal(){
     return this.preco * (1 - this.desc)
@@ -14,5 +15,7 @@ constructor(readonly id: number, readonly nome: string, readonly preco: number, 
 const p1 = new Produto(1,'Caneta',12.5,0.1)
 console.log(p1.precoFinal)
 console.log(p1)
+
+console.log(Validador.ERRO_DESCONHECIDO)
 
 export {}
