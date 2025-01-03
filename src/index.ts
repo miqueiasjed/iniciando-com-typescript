c2d1a4628d2bf54aad4c3c53f25eb8d584eb3ecb
Miqueias Mesquita <miqueias_jed@hotmail.com>
2025-01-03 11:56:19 -0300

    polimorfismo

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,5 @@
 // import './pilares_oo/heranca_2'
 // import './pilares_oo/heranca_3'
 // import './pilares_oo/heranca_4'
-import './pilares_oo/heranca_5'
+// import './pilares_oo/heranca_5'
+import './pilares_oo/polimorfismo'
